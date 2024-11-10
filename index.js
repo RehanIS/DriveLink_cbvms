c8fccbdd59717ab7a12a0301f847eaf52d2ed3b0
Rehan Shaikh <70364417+RehanIS@users.noreply.github.com>
2024-11-10 23:50:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import the new API
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
